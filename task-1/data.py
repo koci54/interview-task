f1595e7496812b74941ea7dc33cb7d96275a4ac4
koci54 <zen.petr@gmail.com>
2021-02-22 10:52:39 +0100

    task1 return data as string

---
+++ task-1/data.py
@@ -0,0 +1,34 @@
+# C20 and C28 contained extra character "
+# I decided to remove this character on both lines
+# I assume that it was a mistype
+# Tab-separated values should not contain these extra charachters
+# please compare data.txt and data2.txt
+''' I decided to work with data2.txt because if we had data coming
+with an extra character, we should probably ensure that this data is fixed
+'''
+
+'''
+# please uncomment this section for comparison
+
+with open('data.txt') as tab_data:
+    print(tab_data.read())
+
+with open('data2.txt') as tab_data2:
+    print(tab_data2.read())
+
+# end comparison
+'''
+
+def main():
+    returned_string_value = open_read_data()
+    print(returned_string_value)
+    print(type(returned_string_value))
+
+def open_read_data():
+    with open('data2.txt') as customers:
+        c =customers.read()
+        # print(c)
+        # print(type(c))
+    return c
+
+main()
